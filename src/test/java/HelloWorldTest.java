3200ea0a6ffa7cc6bb8bd2c2164c4f0632cd9794
ng-22 <golodaeva.natalia@yandex.ru>
2025-01-06 12:28:32 +0300

    Hello from Natalia

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
+public class HelloWorldTest {
+
+    @Test
+    public void TestHelloWorld() {
+        System.out.println("Hello from Natalia");
+    }
+}
