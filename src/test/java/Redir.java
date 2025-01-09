b070b88c18a60122a96df4e4217b8b7e31cb428e
ng-22 <golodaeva.natalia@yandex.ru>
2025-01-09 21:03:11 +0300

    Redir

---
+++ src/test/java/Redir.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Redir {
+
+    @Test
+    public void Redir() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader", "myValue");
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+
+    }
+}
